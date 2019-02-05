5d8aa98ba9c854108c32922b36c84df45674b27e
Dennis <deweis@gmx.ch>
2019-02-05 13:12:01 +0100

    Check if clicked color matches the selected color

--- script.js
+++ script.js
@@ -8,9 +8,25 @@ const colors = [
 ];
 
 const squares = document.querySelectorAll('.square');
-squares.forEach((x, i) => (x.style.backgroundColor = colors[i]));
-
 const pickedColor = colors[3];
 
 const colorDisplay = document.querySelector('#colorDisplay');
 colorDisplay.textContent = pickedColor;
+
+squares.forEach((square, i) => {
+  // Add background color to the squares
+  square.style.backgroundColor = colors[i];
+
+  // Add click listener to the squares
+  square.addEventListener('click', () => {
+    // Grab color of clicked square
+    const clickedColor = square.style.backgroundColor;
+
+    // Compare color to picked color and adjust backgrounds accordingly
+    if (clickedColor !== pickedColor) {
+      square.style.backgroundColor = '#232323';
+    } else {
+      squares.forEach(x => (x.style.backgroundColor = clickedColor));
+    }
+  });
+});
