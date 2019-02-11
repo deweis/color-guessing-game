9bb280bb35e3ca4cb111b711f1e2c82c2853d4e1
Dennis <deweis@gmx.ch>
2019-02-11 12:08:32 +0100

    Refactor code

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 /* The variables in use */
+let countColors = 6;
+let colors = [];
+let pickedColor = '';
+
 const container = document.querySelector('#container');
 const colorDisplay = document.querySelector('#colorDisplay');
 const messageDisplay = document.querySelector('#message');
@@ -7,16 +11,25 @@ const buttonNewGame = document.querySelector('#btnNewGame');
 const buttonEasy = document.querySelector('#btnEasy');
 const buttonHard = document.querySelector('#btnHard');
 const squares = document.querySelectorAll('.square');
-let countColors = 6;
-let colors = generateColors(countColors);
-let pickedColor = colors[getRandomBetween(0, countColors)];
 
-/* Add Event Listeners to the Buttons */
-buttonNewGame.addEventListener('click', newGame);
-buttonEasy.addEventListener('click', () => changeMode('easy'));
-buttonHard.addEventListener('click', () => changeMode('hard'));
+/* Initial Load */
+init();
 
-/* FUNCTION to start a new Game */
+function init() {
+  /* Add Event Listeners to the Buttons */
+  buttonNewGame.addEventListener('click', newGame);
+  buttonEasy.addEventListener('click', () =>
+    countColors === 3 ? newGame() : applyMode(3)
+  );
+  buttonHard.addEventListener('click', () =>
+    countColors === 6 ? newGame() : applyMode(6)
+  );
+
+  /* Start the Game */
+  newGame();
+}
+
+/* Start a new Game */
 function newGame() {
   colors = generateColors(countColors);
   pickedColor = colors[getRandomBetween(0, countColors)];
@@ -28,24 +41,7 @@ function newGame() {
   updateSquares();
 }
 
-/* Start the Game */
-newGame();
-
-/* HELPER FUNCTION: Toggle between EASY and HARD Mode */
-function changeMode(mode) {
-  if (
-    (mode === 'hard' && countColors === 6) ||
-    (mode === 'easy' && countColors === 3)
-  ) {
-    newGame();
-  } else if (mode === 'hard' && countColors === 3) {
-    applyMode(6);
-  } else {
-    applyMode(3);
-  }
-}
-
-/* HELPER FUNCTION: Toggle Easy/Hard button */
+/* Toggle between EASY and HARD Mode */
 function applyMode(num) {
   buttonEasy.classList.toggle('btn-active');
   buttonHard.classList.toggle('btn-active');
@@ -61,53 +57,50 @@ function applyMode(num) {
   newGame();
 }
 
-/* HELPER FUNCTION: Update the squares accordingly with colors */
+/* // Add background color and click listener to the squares */
 function updateSquares() {
   squares.forEach((square, i) => {
-    // Add background color to the squares
     square.style.backgroundColor = colors[i];
-
-    // Add click listener to the squares
-    square.addEventListener('click', () => {
-      // Grab color of clicked square
-      const clickedColor = square.style.backgroundColor;
-
-      // Compare color to picked color and adjust backgrounds accordingly
-      if (clickedColor !== pickedColor) {
-        square.style.backgroundColor = '#232323';
-        messageDisplay.textContent = 'Try Again';
-      } else {
-        messageDisplay.textContent = 'Correct!';
-        buttonNewGame.textContent = 'Play Again?';
-        changeColor(clickedColor);
-      }
-    });
+    square.addEventListener('click', () => verifyColor(square));
   });
 }
 
-/* HELPER FUNCTION: Change the colors of the squares and title background */
+/* Compare color to picked color and adjust backgrounds accordingly */
+function verifyColor(square) {
+  const clickedColor = square.style.backgroundColor;
+  if (clickedColor !== pickedColor) {
+    square.style.backgroundColor = '#232323';
+    messageDisplay.textContent = 'Try Again';
+  } else {
+    messageDisplay.textContent = 'Correct!';
+    buttonNewGame.textContent = 'Play Again?';
+    changeColor(clickedColor);
+  }
+}
+
+/* Change the colors of the squares and title background */
 function changeColor(color) {
   squares.forEach(x => (x.style.backgroundColor = color));
   title.style.backgroundColor = color;
 }
 
-/* HELPER FUNCTION: Create a random number in a range */
+/* Generate a random number in a range */
 function getRandomBetween(min, max) {
   return Math.floor(Math.random() * max + min);
 }
 
-/* HELPER FUNCTION: Generate an array of x random colors */
+/* Generate an array of x random colors */
 function generateColors(num) {
   let arrColors = [];
 
   for (let i = 0; i < num; i++) {
     arrColors.push(getColor());
   }
-  // console.log(arrColors);
+
   return arrColors;
 }
 
-/* HELPER FUNCTION: Generate one random color */
+/* Generate one random color */
 function getColor() {
   const red = getRandomBetween(0, 256);
   const green = getRandomBetween(0, 256);
